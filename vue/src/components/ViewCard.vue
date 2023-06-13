c75ceccdb35a55125c8482d6f989254522953b72
Eric Roberson <fcs2014eroberson@gmail.com>
2023-06-13 15:22:56 -0700

    added view-card component to view-card view

---
+++ vue/src/components/ViewCard.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+    <p></p>
+    I am the view-card component!
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
--- vue/src/views/ViewCard.vue
+++ vue/src/views/ViewCard.vue
@@ -1,10 +1,18 @@
 <template>
 <div>
-    <p>Hellooo I'm working</p>Helloooo I'm working
+    <p>Hellooo I'm working</p>
+    <view-card></view-card>
 </div>
 </template>
 
 <script>
+import ViewCard from '/Users/ericroberson/Desktop/workspace/Recipe-Organizer/Recipe-Organizer-App/vue/src/components/ViewCard.vue';
+export default {
+  name: "home-view",
+  components: {
+    ViewCard
+  }
+};
 </script>
 
 <style>
