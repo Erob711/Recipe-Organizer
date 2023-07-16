f4b19c2e3d3e1b088c7e4356c60245a2e26dfe17
Eric Roberson <fcs2014eroberson@gmail.com>
2023-07-16 14:47:35 -0700

    recipe title on form bound to recipeService POST

--- vue/src/components/CreateNewRecipe.vue
+++ vue/src/components/CreateNewRecipe.vue
@@ -3,17 +3,52 @@
         <!-- TODO: make Create new Recipe form  -->
         <h2>Create New Recipe</h2>
         <form class ="recipe-form">
-            <label for="title">Title:<input type = "text"></label>
-
-
+            <label for="title">Title:<input type = "text" v-model="recipe.title"/></label>
+            <label for="ingredient">Ingredients:<input type = "text"></label>
+            <label for="instructions">Instructions:<input type = "text"></label>
+            <label for="photo-url">Photo Url:<input type = "text"></label>
+            <label for="notes">Notes:<input type = "text"></label>
+            <button type="submit" v-on:click="createRecipe">Submit Updated Listing</button>
         </form>
     </div>
 </template>
 
 <script>
+import recipeService from "/Users/ericroberson/Desktop/workspace/Recipe-Organizer/Recipe-Organizer-App/vue/src/services/RecipeService.js";
+export default {
+    name: "create-recipe-form",
+    data() {
+        return {
+            recipe: {
+                userId: this.$store.state.user.id,
+                title: "",
+                category: ""
+            },
+            ingredients: [
+
+            ],
+            instructions: {
+
+            },
+            photos: [],
+            notes: {
+
+            }
+        }
+    },
+    methods: {
+        createRecipe() {
+            recipeService.createRecipe(this.recipe);
+        }
+    }
+}
 </script>
 
 <style>
+form {
+    display: flex;
+    flex-direction: column;
+}
 .recipe-form {
     margin-top: 5%;
 }
