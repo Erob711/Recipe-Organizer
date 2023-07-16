4d7f9d50e01f92b699ccef2cb819046e5423efb8
Eric Roberson <fcs2014eroberson@gmail.com>
2023-07-16 13:14:36 -0700

    basic form

--- vue/src/components/CreateNewRecipe.vue
+++ vue/src/components/CreateNewRecipe.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
         <!-- TODO: make Create new Recipe form  -->
+        <h2>Create New Recipe</h2>
+        <form class ="recipe-form">
+            <label for="title">Title:<input type = "text"></label>
+
+
+        </form>
     </div>
 </template>
 
@@ -8,4 +14,10 @@
 </script>
 
 <style>
+.recipe-form {
+    margin-top: 5%;
+}
+h2 {
+    margin-top: 10%;
+}
 </style>
