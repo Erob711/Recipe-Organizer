33db060b4605e2b9f4ac73d075296fb729fc6760
Eric Roberson <fcs2014eroberson@gmail.com>
2023-07-18 17:11:06 -0700

    added font awesome and icons to removing ingredients fields

--- vue/src/components/CreateNewRecipe.vue
+++ vue/src/components/CreateNewRecipe.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+        <body>
+        
         <!-- TODO: make Create new Recipe form  -->
         <h2>Create New Recipe</h2>
         <form class ="recipe-form" ref="recipeForm" @submit="createRecipe">
@@ -7,8 +9,11 @@
             <div class = "ingredients" v-for="(input, index) in ingredients" :key="`ingredientInput-${index}`">
                 <label for="ingredient">Ingredients:<input type = "text" v-model="input.ingredient"></label>
                 <div class = "add-and-remove-buttons">
-                    <button type="button" v-on:click="addIngredient(ingredients)">Add Ingredient</button>
-                    <button type="button" v-on:click="removeIngredient(index, ingredients)">Remove Ingredient</button>
+                    <!-- <button type="button" v-on:click="addIngredient(ingredients)">Add Ingredient</button> -->
+                    <!-- <a @click.prevent="addIngredient(ingredients)"><i class="fas fa-minus-circle"></i></a> -->
+                    <a @click.prevent="addIngredient(ingredients)"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg></a>
+                    <a @click.prevent="removeIngredient(index, ingredients)"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/></svg></a>
+                    <!-- <button type="button" v-on:click="removeIngredient(index, ingredients)">Remove Ingredient</button> -->
                 </div>
             </div>
             
@@ -16,8 +21,13 @@
             <label for="photo-url">Photo Url:<input type = "text" v-model="photo.photoUrl"></label>
             <label for="notes">Notes:<input type = "text" v-model="note.note"></label>
             <button type="button" v-on:click="createRecipe">Submit New Recipe</button>
+
+            <!-- TEST -->
+            <!-- TEST -->
         </form>
+        </body>
     </div>
+    
 </template>
 
 <script>
@@ -129,7 +139,9 @@ export default {
         ingredients.push({});
     },
     removeIngredient(index, ingredients){
+        if (ingredients.length > 1) {
         ingredients.splice(index, 1);
+        }
     }
     },
 }
@@ -145,5 +157,8 @@ form {
 }
 h2 {
     margin-top: 10%;
+}
+.fas fa-minus-circle {
+    color: black;
 }
 </style>
